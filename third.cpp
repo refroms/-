f3d30c900670084e30627c064b6a8a24d8a951d8
refroms <100502019+refroms@users.noreply.github.com>
2022-02-27 20:01:11 +0800

    Update third.cpp

--- third.cpp
+++ third.cpp
@@ -1,3 +1,4 @@
+//3. 编写一个函数，输入参数为一个整数（位数未知）和一个指针。要求实现把 这个整数的各个位数存储在指针所指的内存中，并在主函数中输出指针内容。
 #include <iostream>
 using namespace std;
 int *function(int x, int *pp);
@@ -5,13 +6,13 @@ int *function(int x, int *pp);
 int main() {
 	int  *pt, *p;
 	int m, count = 0, i;
-	printf("������һ����\n");
+	printf("请输入一个数\n");
 	cin >> m;
 	p = &m;
 	pt = function(m, p);
 	while (m > 0) {
 		m = m / 10;
-		count ++; //�ܹ�����10������һλ
+		count ++; //能够除以10就增加一位
 	}
 	for (i = 1; i < count + 1; i++) {
 		cout << *(pt + i) << " ";
