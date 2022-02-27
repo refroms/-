e100d23c4f55e0a2c28cf4728d032ffec591fbd4
refroms <100502019+refroms@users.noreply.github.com>
2022-02-27 13:32:14 +0800

    Add files via upload

---
+++ 一.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int *function(int n, int c[]);
+
+int main() {
+	int *p, *pt;
+	int m, i = 0;
+	printf("请输入一个数\n", m);
+	cin >> m;
+	int b[m] = {0};
+	p = b;
+	printf("请输入%d个数\n", m);
+	for (i = 0; i < m; i++) {
+		cin >> *(p + i);
+	}
+	pt = function(m, b);
+	for (i = 0; i < m; i++) {
+		cout << *(pt + i) << " ";
+	}
+	return 0;
+}
+
+int *function(int n, int c[]) {
+	int *pp;
+	int i, j;
+	pp = c;
+	int temp = 0;
+	for (int i = 0 ; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (*(pp + i) < * (pp + j)) {
+				temp = *(pp + i);
+				*(pp + i) = *(pp + j);
+				*(pp + j) = temp;
+			}
+		}
+	}
+	return pp;
+}
